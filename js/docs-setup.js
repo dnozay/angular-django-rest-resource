799e9b56a8696e2953866611b42650a4d7068e61
Damien Nozay <damien.nozay@gmail.com>
2014-12-05 12:15:12 -0800

    Built angular-django-rest-resource from commit 0290698 on branch docs

---
+++ js/docs-setup.js
@@ -0,0 +1,34 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "djangoRESTResources",
+      "shortName": "djangoRESTResources",
+      "type": "overview",
+      "moduleName": "djangoRESTResources",
+      "shortDescription": "",
+      "keywords": "api djangorestresources overview"
+    },
+    {
+      "section": "api",
+      "id": "djangoRESTResources.djResource",
+      "shortName": "djangoRESTResources.djResource",
+      "type": "object",
+      "moduleName": "djangoRESTResources",
+      "shortDescription": "A factory for generating classes that interact with a Django REST Framework backend.",
+      "keywords": "$action $cachefactory $http $q $resolved $resource $save $then _object_ abc action action2 actions actual additional allows angular angular-django-rest-resource angularjs api app appended array assume assumed backend body bound built cache caching call callback callbacks called calling class classes collection config count created credentials crud custom data data-binding declaration default deferred delete depending derived deserialized destination django djangorestresource djangorestresources djresource easily empty endpoint error excess executed existing extend extended extracted factory field flag format framework function functions generating greet hash headers headersgetter hello http https identical include inject installation instance instances interact invoke invoked invoking isarray js json jsonp key knowing load method method_if_field_has_value methods milliseconds module mozilla ngresource non-get number object occurs operation operations optional optionally org overridden override paginate_by param paramdefaults parameter parameters parametrized params perform populated port post postdata pre-bound prefix prefixed promise properties property query read receives reference rejection remove request requests requesttype resolved resource resource-level resources respected response responsetype rest result returned returns salutation save search section_5 serialized server server-side set single slashes specific streamed succeeds success supported takes template templating things time timeout trailing transform transformed transformrequest transformresponse true underlying update url urls user valid values var version withcredentials xhr"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": true,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
---
+++ pages.js
@@ -0,0 +1,8 @@
+NG_DOCS={
+  "sections": {},
+  "pages": [],
+  "apis": {},
+  "title": "angular-django-rest-resource",
+  "html5Mode": true,
+  "startPage": "/api"
+};
